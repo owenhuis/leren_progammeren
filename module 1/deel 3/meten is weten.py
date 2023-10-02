da65c7700d0f05d4a97ab7846469c6d827b8dd15
owenhuis <143698007+owenhuis@users.noreply.github.com>
2023-10-02 15:03:52 +0200

    min en max
    is wel makelijk te doen

--- module 1/deel 3/meten is weten.py
+++ module 1/deel 3/meten is weten.py
@@ -1,13 +1,17 @@
 a = int(input('welk heel getal wil je hebben?  '))
 b = int(input('welk tweede hele getal wil je?  '))
 
-if a >= b:
+if a > b:
     maxwaarde = a
     print(f'{a} = het groostste getal!')
     print(f'de max waarde is {maxwaarde}')
-elif a <= b:
+elif a < b:
     minwaarde = a
     print(f'{a} is het kleinste getal!')
     print(f'de min waarde = {minwaarde}')
 else:
     print(f'{a} = gelijk aan {b}!')
+    minwaarde = {a}
+    maxwaarde = {b}
+    print(f'het maximum getal = {maxwaarde}')
+    print(f'het mininum getal = {minwaarde}')
