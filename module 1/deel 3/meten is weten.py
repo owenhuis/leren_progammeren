1bdd83a11240ab6d159b75debe8b0f2a3860ee02
owenhuis <143698007+owenhuis@users.noreply.github.com>
2023-10-02 14:54:11 +0200

    elif-statement
    deel 3 1-2 is wel goed te doen

--- module 1/deel 3/meten is weten.py
+++ module 1/deel 3/meten is weten.py
@@ -1,6 +1,11 @@
 a = int(input('welk heel getal wil je hebben?  '))
-b = int(input('welk twede hele getal wil je?   '))
+b = int(input('welk tweede hele getal wil je?  '))
 
 if a >= b:
-    
-    print(f'{a} = het groostste getal')
+    maxwaarde = a
+    print(f'{a} = het groostste getal!')
+    print(f'de max waarde is {maxwaarde}')
+elif a <= b:
+    minwaarde = a
+    print(f'{a} is het kleinste getal!')
+    print(f'de min waarde = {minwaarde}')
