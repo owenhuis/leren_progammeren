8c9456de087872efeefcd9a678724297ba76223c
owenhuis <143698007+owenhuis@users.noreply.github.com>
2023-10-25 11:18:38 +0200

    update
    er is geen foutmelding meer :)

--- module 1/deel 4/order_bug.py
+++ module 1/deel 4/order_bug.py
@@ -6,12 +6,10 @@ if getal_2 == 0:
 else:
     print ("{} gedeeld door {} is gelijk aan {}".format(getal_1, getal_2, resultaat))
 
-
-
 def vraag_getal(aantal):
     antwoord = input("Voer het "+ antwoord +" getal in: ")
     getal = int(aantal)
     return getal
 
 def deel_getallen(a, b):
-    return antwoord
+    return a / b
