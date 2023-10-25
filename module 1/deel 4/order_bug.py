5bfa3bb8b4cbede51c7fe59262864a3862737155
owenhuis <143698007+owenhuis@users.noreply.github.com>
2023-10-25 14:37:29 +0200

    order_bug
    nagekeken door meneer corbijn de woorden opdracht is nog fout

--- module 1/deel 4/order_bug.py
+++ module 1/deel 4/order_bug.py
@@ -1,15 +1,19 @@
-getal_1 = int(input('hoeveel is getal 1?  '))
-getal_2 = int(input('hoeveel is getal 2?  '))
-resultaat = getal_1 / getal_2
-if getal_2 == 0:
-    print("Kan niet delen door 0")
-else:
-    print ("{} gedeeld door {} is gelijk aan {}".format(getal_1, getal_2, resultaat))
-
 def vraag_getal(aantal):
-    antwoord = input("Voer het "+ antwoord +" getal in: ")
-    getal = int(aantal)
+    antwoord = input(f"Voer het {aantal} getal in: ")
+    getal = int(antwoord)
     return getal
 
 def deel_getallen(a, b):
-    return a / b
+    antwoord = (a / b)
+    return antwoord
+
+getal_1 = vraag_getal("eerste")
+getal_2 = vraag_getal("tweede")
+
+
+if getal_2 == 0:
+    print("Kan niet delen door 0")
+else:
+    resultaat = deel_getallen(getal_1, getal_2)
+    print ("{} gedeeld door {} is gelijk aan {}".format(getal_1, getal_2, resultaat))
+    
