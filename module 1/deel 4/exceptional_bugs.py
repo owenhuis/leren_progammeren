0f8fa33d11ddd03bf23e91b4667fa74a365398bf
owenhuis <143698007+owenhuis@users.noreply.github.com>
2023-10-26 14:50:45 +0200

    nakijken klaar en begin deel 5

--- module 1/deel 4/exceptional_bugs.py
+++ module 1/deel 4/exceptional_bugs.py
@@ -5,13 +5,14 @@ num1 = int(random.randint(1,10))
 num2 = int(random.randint(5,15))
 
 #vraag om een antwoord
-number = int(input(f"Weet jij wat {num1} + {num2} is? ")) 
-
-#geef reactie op het antwoord
 try:
-    if int(number == num1+num2):
-        print('Dat is juist')
-    elif int(number != num1 + num2):
-        print('Nee dat klopt niet')
+    number = int(input(f"Weet jij wat {num1} + {num2} is? ")) 
 except ValueError:
     print('Dat is geen nummer!')
+else:
+#geef reactie op het antwoord
+    if number == num1+num2:
+        print('Dat is juist')
+    else:
+        print('Nee dat klopt niet')
+
--- module 1/deel 4/exeptinol pizzacalculator.py
+++ module 1/deel 4/exeptinol pizzacalculator.py
@@ -8,7 +8,9 @@ try:
     pizza_small_count = int(input("Hoeveel small pizza's wil je? "))
     pizza_medium_count = int(input("Hoeveel medium pizza's wil je? "))
     pizza_large_count = int(input("Hoeveel large pizza's wil je? "))
-    
+except ValueError:
+    print('Voer een geldig getal in')
+else:       
     # Bereken de prijs individueel
     pizza_small_price = small_pizza_prijs * pizza_small_count
     pizza_medium_price = medium_pizza_prijs * pizza_medium_count
@@ -26,5 +28,4 @@ try:
     print(f"Totale prijs: {totaal_bedrag:.2f} euro")
     print("-----------------------------")
     print()
-except ValueError:
-    print('Voer een geldig getal in')
+
