ec016abdda3e5c146e8a9a020c9c9feed9e105e8
owenhuis <143698007+owenhuis@users.noreply.github.com>
2023-12-01 09:04:16 +0100

    dagen done

--- module 2/deel 2/dagen.py
+++ module 2/deel 2/dagen.py
@@ -1,7 +1,19 @@
-weekdagen = 'maandag','dinsdag','woensdag','donderdag','vrijdag','zaterdag','zondaag'
+WEEKDAGEN = 'maandag','dinsdag','woensdag','donderdag','vrijdag','zaterdag','zondag'
 
-for dag_in_de_week in weekdagen:
+for dag_in_de_week in WEEKDAGEN:
     print(dag_in_de_week, end=' ')
 print()
-for werkdagen in  range(len(weekdagen)):
-    print(weekdagen[werkdagen])
+for werkdagen in WEEKDAGEN[:5]:
+    print(werkdagen, end=' ')
+print()
+for weekenddagen in WEEKDAGEN[5:]:
+    print(weekenddagen, end=' ')
+print()
+for omgekeerd in reversed(WEEKDAGEN):
+    print(omgekeerd, end=' ')
+print()
+for omgekeerd_werk in reversed(WEEKDAGEN[:5]):
+    print(omgekeerd_werk, end=' ')
+print()
+for omgekeerd_weekend in reversed(WEEKDAGEN[5:]):
+    print(omgekeerd_weekend, end=' ')
