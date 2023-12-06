135240aaab09f9e5140c2568c7a02236174fab4b
owenhuis <143698007+owenhuis@users.noreply.github.com>
2023-12-06 10:26:50 +0100

    update the list factory

--- module 2/deel 2/the list factory v2 (dictioneries na uilteg daarvan).py
+++ module 2/deel 2/the list factory v2 (dictioneries na uilteg daarvan).py
@@ -5,7 +5,8 @@ for i in range(1, hoeveel + 1):
     lijst = {}
     aantal_nummers = int(input(f'hoeveel nummers wil je in lijstje {i}?'))
     lijst['nummers'] = list(range(i, aantal_nummers * i + 1, i))
-    the_list.append(lijst)
+    numerlijst = list(lijst['nummers'])
+    the_list.append(numerlijst)
 
-for getallen in the_list:
-    print(getallen)
+for nummers in the_list:
+    print(nummers, end=' ')
