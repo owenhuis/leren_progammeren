998d41b305b37cf9d6ce76b01fdbe32d347941bb
owenhuis <143698007+owenhuis@users.noreply.github.com>
2023-12-08 11:48:58 +0100

    kaartspel done

--- module 2/deel 2/deck van kaartspellen.py
+++ module 2/deel 2/deck van kaartspellen.py
@@ -1,22 +1,16 @@
 import random
-vormen = ['harten', 'klaveren', 'schoppen', 'ruiten']
-nummer = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-volle_dek = ['joker','joker']
+vormen = ['harten', 'klaver', 'schoppen', 'ruiten']
+nummers = ['aas',2,3,4,5,6,7,8,9,10,'boer','vrouw','heer']
+volle_deck = ['joker','joker']
 
-for dek in range(52):
-    #speciale caracters
-    if nummer == 11:
-        nummer = 'boer'
-    elif nummer == 12:
-        nummer = 'koningning'
-    elif nummer == 13:
-        nummer = 'koning'
-    elif nummer == 1:
-        nummer = 'aas'
-    kaart = f'{vormen}  {nummer}'
-    volle_dek.append(kaart)
+for vorm in vormen:
+    for nummer in nummers:
+        kaart = vorm + " " + str(nummer)
+        volle_deck.append(kaart)
+
+hand = random.shuffle(volle_deck)
 for hand in range(7):
-    hand = random.shuffle(volle_dek)
-    handje = hand.pop(volle_dek)
-    print(handje)
-print('deck (47 kaarten): ', volle_dek)
+    handje = volle_deck.pop(0)
+    print(f'kaart {hand+1}: {handje}')
+
+print('deck (47 kaarten): ', volle_deck)
