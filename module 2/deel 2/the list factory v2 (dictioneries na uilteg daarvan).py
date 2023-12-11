3e4a6e0d4149fc3b12be1704118d5a77822cbab7
owenhuis <143698007+owenhuis@users.noreply.github.com>
2023-12-11 14:37:36 +0100

    cjv klaar

--- module 2/deel 2/the list factory v2 (dictioneries na uilteg daarvan).py
+++ module 2/deel 2/the list factory v2 (dictioneries na uilteg daarvan).py
@@ -4,9 +4,8 @@ hoeveel = int(input('hoeveel lijstjes wil je?'))
 for i in range(1, hoeveel + 1):
     lijst = {}
     aantal_nummers = int(input(f'hoeveel nummers wil je in lijstje {i}?'))
-    lijst['nummers'] = list(range(i, aantal_nummers + aantal_nummers + 1, 2))
+    lijst['nummers'] = list(range(i, aantal_nummers + aantal_nummers + i, 2))
     numerlijst = list(lijst['nummers'])
     the_list.append(numerlijst)
-
 
 print(the_list)
