acfde330257c8bf5f34a5d7304977966ab9735ae
owenhuis <143698007+owenhuis@users.noreply.github.com>
2023-12-13 11:55:34 +0100

    text ge edit

--- module 2/deel 2/text-eidtor.py
+++ module 2/deel 2/text-eidtor.py
@@ -1,15 +1,22 @@
+from time import sleep
 vertaal_woorden = {
-    'hoi'
-    'doei'
-    'blub'
-    'piraat'
-    'vogel'
+    'hoi': 'doei',
+    'doei': 'hoi',
+    'blub': 'blaf',
+    'piraat': 'olifant',
+    'vogel': 'mens',
 }
 
-vertaalde_woorden = {
-    'doei'
-    'hoi'
-    'blaf'
-    'olifant'
-    'mens'
-}
+zin = 'de piraat zij hoi tegen zijn vogel terwijl de vis blub en doei zij daar schrok de olifant van'
+gespleete_zin = zin.split()
+vertaalde_zin = ' '.join(vertaal_woorden.get(woord, woord)for woord in gespleete_zin)
+
+print('de eerste zin:')
+sleep(1)
+print(zin)
+sleep(1)
+print('')
+sleep(2)
+print('vertaalde zin')
+sleep(1)
+print(vertaalde_zin)
