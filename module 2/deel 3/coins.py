d16996ce51774c740aa9902828491be409db27a2
owenhuis <143698007+owenhuis@users.noreply.github.com>
2024-01-10 09:55:10 +0100

    update

--- module 2/deel 3/coins.py
+++ module 2/deel 3/coins.py
@@ -4,27 +4,28 @@
 # function of program:
 # structure of program: 
 
-toPay = int(float(input('Amount to pay: '))* 100) #
-paid = int(float(input('Paid amount: ')) * 100) #
-change = paid - toPay #
+toPay = int(float(input('Amount to pay: '))* 100) #input betalen
+paid = int(float(input('Paid amount: ')) * 100) #input gegeven
+change = paid - toPay #change berekenen
 
-if change > 0: #
-  coinValue = 5.00 #
+if change > 0: # start if
+  coinValue = 500 #de hoogste value coin voor in een latere berekening
   
-  while change > 0 and coinValue > 0: #
-    nrCoins = change // coinValue #
+  while change > 0 and coinValue > 0: #start while loop
+    nrCoins = change // coinValue #berekening change hoeveelhijd
+    
+    if nrCoins > 0: #start voor een berekening en print met een ifp
+      print('return maximal ', nrCoins, ' coins of ', coinValue, ' cents!' ) # je print de hoeveelheid dat terug moet worden gegeven en de coin grootte
+      nrCoinsReturned = int(input('How many coins of ' + str(coinValue) +  ' cents did you return? ')) # input hoeveel munten je terug geeeft
+      change -= nrCoinsReturned * coinValue #berekening om de change hoeveelhijd om laag te halen en om daarna als er nog change over is dat er dan opnieuw de loop wordt gedaan
+    
 
-    if nrCoins > 0: #
-      print('return maximal ', nrCoins, ' coins of ', coinValue, ' cents!' ) #
-      nrCoinsReturned = int(input('How many coins of ' + str(coinValue) +  ' cents did you return? ')) #
-      change -= nrCoinsReturned * coinValue #
-
-# comment on code below: 
-    if coinValue == 5.00:
-      coinValue = 2.00
-    elif coinValue == 2.00:
-      coinValue = 1.00
-    elif coinValue == 1.00:
+# comment on code below: als de coinvalue een bepaalde grote heeft wordt het een kleinere grote
+    if coinValue == 500:
+      coinValue = 200
+    elif coinValue == 200:
+      coinValue = 100
+    elif coinValue == 100:
       coinValue = 50
     elif coinValue == 50:
       coinValue = 20
@@ -39,7 +40,7 @@ if change > 0: #
     else:
       coinValue = 0
 
-if change > 0: #
-  print('Change not returned: ', str(change) + ' cents')
-else:
+if change > 0: # als de change groter is dan 0 dan wordt er aan gegeven dan print er welke change je mist anders print er done
+  print('Change not returned: ', change / 100,' cents')
+elif change <= 0:
   print('done')
--- module 2/deel 3/tot 1000.py
+++ module 2/deel 3/tot 1000.py
@@ -1,9 +1,24 @@
-hoeveelheid = 50
-toevoegen = 51
-while hoeveelheid < 1000:
-    hoeveelheid = hoeveelheid + toevoegen
-    print(hoeveelheid)
-    toevoegen += 1
-    if hoeveelheid >= 1000:
-        print('nu heb je meer dan of duizend getallen')
-        break
+# start_getal = 50
+# hoeveelheid = 50
+# toevoegen = 51
+
+# print(hoeveelheid)
+# while hoeveelheid < 1000:
+#     print(f'{start_getal} + {toevoegen} = {hoeveelheid}')
+#     hoeveelheid = hoeveelheid + toevoegen
+#     toevoegen += 1
+#     if hoeveelheid >= 1000:
+#         print('nu heb je meer dan of duizend getallen')
+#         break
+
+
+total_sum = 0
+current_number = 50
+iteration = 1
+
+while total_sum <= 1000:
+    total_sum += current_number
+    print(f"{iteration}. {'+'.join(map(str, range(50, current_number+1)))} = {total_sum}")
+    
+    current_number += 1
+    iteration += 1
