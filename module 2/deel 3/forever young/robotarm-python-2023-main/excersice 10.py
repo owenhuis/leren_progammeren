82780d62d5446eb317b1f46908b16b4af0c4b419
owenhuis <143698007+owenhuis@users.noreply.github.com>
2024-01-10 14:09:17 +0100

    excersice 9

--- module 2/deel 3/forever young/robotarm-python-2023-main/excersice 10.py
+++ module 2/deel 3/forever young/robotarm-python-2023-main/excersice 10.py
@@ -3,7 +3,7 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 7')
 
 # Jouw python instructies zet je vanaf hier:
- 
+
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
