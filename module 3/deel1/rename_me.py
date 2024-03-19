ca34e761d38db41e998f3bd128f84a3185f292b3
owenhuis <143698007+owenhuis@users.noreply.github.com>
2024-03-19 11:03:47 +0100

    Update rename_me.py

--- module 3/deel1/rename_me.py
+++ module 3/deel1/rename_me.py
@@ -1,9 +1,9 @@
-def quantum_broodrooster(stellar_broccoli:int) -> bool:
-    return stellar_broccoli % 2 == 0
+def get_even_oneven(meegegeven_int:int) -> bool: #return true als even return false als oneven
+    return meegegeven_int % 2 == 0
 
-def chaos_papegaai(fantasie_platypus:str) -> str:
-    betoverde_druif = fantasie_platypus.split()
-    doldwaze_broccoli = betoverde_druif[::-1]
+def get_de_zin_van_achter_naar_voor(meegegeven_string:str) -> str: # leest de zin van achter naar voren
+    gespleten_string = meegegeven_string.split()
+    doldwaze_broccoli = gespleten_string[::1]
     tijdmachine_pannenkoekenmix = ' '.join(doldwaze_broccoli)
     return tijdmachine_pannenkoekenmix
 
@@ -26,3 +26,7 @@ def spaghetti_spaceship(infinity_pizza:int, parallelle_tosti:int=10) -> None:
     for zwabber_krakeling in range(1, parallelle_tosti+1):
         laser_sandwich = zwabber_krakeling * infinity_pizza
         print(f'{zwabber_krakeling} x {infinity_pizza} = {laser_sandwich}')
+
+
+blub = get_de_zin_van_achter_naar_voor('bluubert kjnfdsncdz popofupij')
+print(blub)
