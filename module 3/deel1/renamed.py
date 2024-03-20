d85fd895209e911af7a0cc875c702c51b9264d35
owenhuis <143698007+owenhuis@users.noreply.github.com>
2024-03-20 14:16:04 +0100

    module 3 deel 1 opdracht 1 done

---
+++ module 3/deel1/renamed.py
@@ -0,0 +1,31 @@
+def get_even_oneven(meegegeven_int:int) -> bool: #return true als even return false als oneven
+    return meegegeven_int % 2 == 0
+
+def get_omgekeerde_zin(meegegeven_string:str) -> str: # draait je zin om
+    gespleten_string = meegegeven_string.split()
+    omgekeerde_woordenlijst = gespleten_string[::-1]
+    gejoinde_string = ' '.join(omgekeerde_woordenlijst)
+    return gejoinde_string
+
+def get_aantal_unieke_letters_in_string(meegegeven_string:str) -> int: # geeft je de unieke letters in je string
+    unieke_letters_dict = set(meegegeven_string)
+    aantalk_unieke_letters = len(unieke_letters_dict)
+    return aantalk_unieke_letters
+
+def get_gemiddelde_lengte_zin(megegeven_str:str) -> float:
+    gespleten_string = megegeven_str.split()
+    aantal_woorden = 0
+
+    for woord in gespleten_string:
+        aantal_woorden += len(woord)
+        print(aantal_woorden)
+
+    gemiddelde_lengte_woorden = aantal_woorden / len(gespleten_string)
+    return gemiddelde_lengte_woorden
+
+def get_tafel(meegegeven_int:int, nummer_tien:int=10) -> None:
+    for linker_getal_in_som in range(1, nummer_tien+1):
+        berekening = linker_getal_in_som * meegegeven_int
+        print(f'{linker_getal_in_som} x {meegegeven_int} = {berekening}')
+
+
